54aa2bc63d24e878e9cfa7d5ce8074eb6604038f
‘samorobo’ <samgodwin111@gmail.com>
2022-04-12 13:54:12 -0700

    My jack bauer task commit

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,24 +1,31 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * jack_bauer - Check Holberton
- * Description: function prints every minute in one day
- * Return: Nothing
+ * jack_bauer -prints every minute of the day
+ *
+ * Return: Always 0.
  */
-void jack_bauer(void)
-{
-	int H, M;
 
-	for (H = 0; H < 24; H++)
-	{
-		for (M = 0; M < 60; M++)
-		{
-			_putchar((H / 10) + '0');
-			_putchar((H % 10) + '0');
-			_putchar(':');
-			_putchar((M / 10) + '0');
-			_putchar((M % 10) + '0');
-			_putchar('\n');
-		}
-	}
+void jack_bauer(void)
+
+{
+int a;
+int b;
+
+for (a = 0; a <= 23; a++)
+{
+for (b = 0; b <= 59; b++)
+{
+_putchar (a / 10 + '0');
+_putchar (a % 10 + '0');
+_putchar (':');
+_putchar (b / 10 + '0');
+_putchar (b % 10 + '0');
+_putchar ('\n');
+
+}
+
+
+}
+
 }
