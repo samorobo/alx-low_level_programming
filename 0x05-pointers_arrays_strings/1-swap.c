fa021a973f60bd981dd6bfc18bc8edbb7235940a
‘samorobo’ <samgodwin111@gmail.com>
2022-04-20 04:54:18 -0700

    My first commit

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * swap_innt - swaps the value of two integers
+ * @a: An integer to swap
+ * @b: Another integer to swap
+ */
+
+void swap_int(int*a, int*b)
+{
+	int s = *a;
+	*a = *b;
+	*b = c;
+}
