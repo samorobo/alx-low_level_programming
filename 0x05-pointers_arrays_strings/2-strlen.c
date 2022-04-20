23daf05734578b6b68e1898c362e1b50e27ad416
‘samorobo’ <samgodwin111@gmail.com>
2022-04-20 08:54:32 -0700

    third commit

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of the string
+ * @s: input string
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	int c = 0;
+	while(*s + c != '\0')
+	{
+		c++;
+	}
+	return (c);
+}
