a34f476f59563892f85ecf2c7ae843737a5e4fe9
‘samorobo’ <samgodwin111@gmail.com>
2022-04-20 10:18:08 -0700

    third commit

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - printing a string followed by a new line
+ * @str: string to be printed
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	for (str = 0; str[f]; f++)
+	{
+		_putchar(str[f]);
+	}
+	_putchar('\n');
+}
+
