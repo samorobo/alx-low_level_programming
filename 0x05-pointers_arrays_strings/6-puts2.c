6ac056e4c379480fc9d3f474d26d988f0dd7ce86
‘samorobo’ <samgodwin111@gmail.com>
2022-04-20 17:17:03 -0700

    sixth commit

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * puts2 - print every other character of a string
+ * @str: input string
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	int b;
+
+	for (b = 0; str[b] != '\0'; b++)
+	{
+		if (b % 2 == 0)
+			_putchar(str[b]);
+	}
+	_putchar('\n');
+}
