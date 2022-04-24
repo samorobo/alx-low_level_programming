dc25aeffc5020f399eb0d1f221d33a24f7470baa
‘samorobo’ <samgodwin111@gmail.com>
2022-04-24 08:54:46 -0700

    My zeroth commit

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * *_strcat - concatenates two strings
+ * @dest: Destination string
+ * @src: Source string
+ * Return: dest
+ */
+
+char *_strcat(char *dest char *src)
+{
+	int i, j;
+	strcat(dest[i],src[j]);
+	dest[i] = '\0';
+	return(dest);
+}
