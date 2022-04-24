4d6470e86a064b26f1b238c9d42c9c4a408cb9e2
‘samorobo’ <samgodwin111@gmail.com>
2022-04-24 10:56:49 -0700

    My first commit

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,33 +1,32 @@
 #include "main.h"
-
 /**
- * *_strncat - Concatenates two strings
- * @dest: Destination string
- * @src: Source string
- * @n: limit of the values to be concatenated
- * Return: dest
+ *_strncat - concatenates two strings
+ *@dest: A pointer to a character that will be changed
+ *@src: A pointer to a character that will be changed
+ *@n: value
+ *Return: dest
  */
 
-char *_strncat(char *dest char *src int n)
+char *_strncat(char *dest, char *src, int n)
+
 {
-	int i, j;
+int i, j;
 
-	i = 0;
-	while(dest[i] != '\0')
-	{
-		i++;
-	}
-
-	j = 0;
-	while(j < n && src[j] != '\0')
-	{
-		dest[i] = src[j];
-		j++;
-		i++;
-	}
-
-	dest[i] = '\0';
-
-	return (dest);
+i = 0;
+while (dest[i] != '\0')
+{
+i++;
 }
 
+j = 0;
+while (j < n && src[j] != '\0')
+{
+dest[i] = src[j];
+j++;
+i++;
+}
+
+dest[i] = '\0';
+
+return (dest);
+}
