f205386dbe83d03c50ec98a758e715550505e25f
‘samorobo’ <samgodwin111@gmail.com>
2022-04-26 15:53:00 -0700

    My fourth commit

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -9,11 +9,11 @@
 
 char *_strpbrk(cjhar *s, char *accept)
 {
-	int i, j;
+	unsigned int i, j;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; accept[j] = '\0'; j++)
+		for (j = 0; accept[j] != '\0'; j++)
 		{
 			if (s[i] == accept[j])
 
