a52c8f8875692e36ee6a038e6d865e0757346f07
‘samorobo’ <samgodwin111@gmail.com>
2022-04-26 09:25:28 -0700

    My first commit

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies memory area
+ * @dest: destination to be copied into
+ * @src: memory source
+ * @n: bytes in the memory
+ * Return: pointer to dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
+
+	return (dest);
+}
