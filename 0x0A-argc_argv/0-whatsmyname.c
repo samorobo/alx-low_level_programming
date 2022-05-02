02bfbc9e2aac1b4373fe5d5d75643ff34947cc84
‘samorobo’ <samgodwin111@gmail.com>
2022-05-02 05:10:02 -0700

    My zroth commit

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,16 +2,17 @@
 #include "main.h"
 
 /**
- * main - prints the name of the program
- * @argc: Argument count
- * @argv: Argument vector
+ * main - Print the name of the program
+ * @argc: Count arguments
+ * @argv: Arguments
+ *
  * Return: Always 0 (Success)
  */
 
-int main(int argc, char argc[])
+int main(int argc, char *argv[])
 {
-	(void) argc;
-	printf("%s\n", argv[0]);
+(void) argc;
+printf("%s\n", argv[0]);
 
-	return (0);
+return (0);
 }
