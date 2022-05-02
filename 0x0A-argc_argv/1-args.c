248bc8c893dbf939f16cc02ac01a9aac54697be4
‘samorobo’ <samgodwin111@gmail.com>
2022-05-02 06:00:57 -0700

    My second commit

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints number of argument pasdsed into it
+ * @argc: Argument count
+ * @argv: Argument vector
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char argv[])
+{
+	(void) argv;
+
+	printf("%d", argc - 1);
+
+	return (0);
+}
