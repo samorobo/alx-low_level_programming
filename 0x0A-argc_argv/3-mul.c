4c662190a110166ffa9bd761b8ab457a64f4a12b
‘samorobo’ <samgodwin111@gmail.com>
2022-05-02 08:43:34 -0700

    My third commit

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -8,14 +8,13 @@
  * Return 0 if true, else return 1
  */
 
-int main(int argc; char *argv)
+int main(int argc, char *argv)
 {
 	if (if argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	else
 		printf("%d\n"' atoi(argv[1]) * atoi(argv[2]));
 	return (0);
 }
