7b1721c0d5035215fb58900cb89b817cb5f36cdc
‘samorobo’ <samgodwin111@gmail.com>
2022-05-02 06:38:47 -0700

    My second commit

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,19 +1,15 @@
 #include <stdio.h>
-#include "main.h"
-
 /**
- * main - prints all the arguments ita recieves
- * @argc: Argument count
- * @argv: Argument vector
- *
- * Return: Always 0 (Success)
+ * main - prints all arguments it receives.
+ * @argc: number of command line arguments.
+ * @argv: array that contains the program command line arguments.
+ * Return: 0 - success.
  */
-
-int main(char argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	int i;
 
 	for (i = 0; i < argc; i++)
-		printf("%s\n"' argv[i]);
+		printf("%s\n", argv[i]);
 	return (0);
 }
