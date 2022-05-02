3a2aade03b4ef1fb34f7ab15f9aa5a3bbf761c74
‘samorobo’ <samgodwin111@gmail.com>
2022-05-02 08:40:58 -0700

    my third commit

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,21 @@
+#include <stdio>
+#include "main.h"
+
+/**
+ * main - program that multiplies two numbers
+ * @argc: Argument count
+ * @argv: Argument vector
+ * Return 0 if true, else return 1
+ */
+
+int main(int argc; char *argv)
+{
+	if (if argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+		printf("%d\n"' atoi(argv[1]) * atoi(argv[2]));
+	return (0);
+}
