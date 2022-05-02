b300a0d02e370c883bd4c1729d86fa2549ae9699
‘samorobo’ <samgodwin111@gmail.com>
2022-05-02 04:59:20 -0700

    My zeroth commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - prints the name of the program
+ * @argc: Argument count
+ * @argv: Argument vector
+ * Return: Always 0
+ */
+
+int main(int argc, char argc[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
