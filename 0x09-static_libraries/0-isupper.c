a18886937cefbc2c3cd70552f10501cc3d3eef80
‘samorobo’ <samgodwin111@gmail.com>
2022-05-04 10:26:53 -0700

    My first static library commit

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -1,11 +1,20 @@
+uth: Brennan D Baraban
+ */
+
 #include "main.h"
 
-/**
- * _isupper - checks for uppercase character
- * @c: the character to be checked
- * Return: 1 if c is uppercase, 0 otherwise
- */
-int _isupper(int c)
+ /**
+  *  * _isupper - Checks for uppercase characters.
+  *   * @c: The character to be checked.
+  *    *
+  *     * Return: 1 if character is uppercase, 0 otherwise.
+  */
+ int _isupper(int c)
 {
-	return (c >= 'A' && c <= 'Z');
+		if (c >= 'A' && c <= 'Z')
+					return (1);
+
+			else
+						return (0);
 }
+
