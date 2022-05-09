5fece1e4218b66e7d3f56d1507fa9e38a00bd8d0
‘samorobo’ <samgodwin111@gmail.com>
2022-05-09 08:11:03 -0700

    My first commit

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,45 +1,51 @@
 #include "main.h"
-#include <stdlib.h>
 
 /**
- * string_nconcat - concatenates two strings
+ * string_nconcat - concatenates two strings.
  * @s1: first string
  * @s2: second string
- * @n: amount of bytes
+ * @n: index
  * Return: char pointer
  */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *p;
-	unsigned int size1, size2, k;
+	unsigned int size1 = 0, size2 = 0, i;
 
 	if (s1 == NULL)
 		s1 = "";
+
 	if (s2 == NULL)
 		s2 = "";
 
-	for (size1 = 0; s1[size1] != '\0'; size1++)
-		;
-	for (size2 = 0; s2[size2] != '\0'; size2++)
-		;
+	while (s1[size1] != '\0')
+	{
+		size1++;
+	}
 
-	if (n > s2)
-		n = size2;
-	p = malloc(size1 + n + 1) * sizeof(char));
+	while (s2[size2] != '\0')
+	{
+		size2++;
+	}
+
+	if (n > size2)
+	n = size2;
+	p = malloc((size1 + n + 1) * sizeof(char));
 
 	if (p == NULL)
 		return (0);
 
-	for (k = 0; k < size1; k++)
+	for (i = 0; i < size1; i++)
 	{
-		p[k] = s1[k];
+		p[i] = s1[i];
 	}
-	for (; k < (size1 + n); k++)
-	{
-		p[k] = s2[k - size1];
-	}
-	p[k] = '\0';
 
-	return (p);
+	for (; i < (size1 + n); i++)
+	{
+		p[i] = s2[i - size1];
+	}
+	p[i] = '\0';
+
+return (p);
 }
