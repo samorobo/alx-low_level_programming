8109dd1517db2c80c4a965a4500ae3cb44bea3a0
‘samorobo’ <samgodwin111@gmail.com>
2022-05-09 04:31:06 -0700

    My zeroth commt

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocates memory
+ * @b: size to be allocated
+ * Return: returns pointer to the allocated memory.
+ * if malloc fails,status value is equal to 98
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	int *p;
+
+	p = malloc(p);
+	if (p == NULL)
+	{
+		exit(98);
+	}
+	return (98);
+}
