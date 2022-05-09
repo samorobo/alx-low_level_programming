a88d95d2d7490bfb7ebb11326c21b4b1c54f709d
‘samorobo’ <samgodwin111@gmail.com>
2022-05-09 09:28:33 -0700

    My second commit

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _calloc - allocates memory
+ * @nmemb: number of elements in array
+ * @size: bytes of each array in position in array
+ * Return: pointer void
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *p;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+
+	p = malloc(nmemb * size);
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+	for ( i = 0; i < (nmemb * size); i++)
+	{
+		p[i] = 0;
+	}
+	return (p);
+}
