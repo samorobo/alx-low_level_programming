7fd83b1816e3eebf5931320f50703611e9ddc178
‘samorobo’ <samgodwin111@gmail.com>
2022-05-24 14:22:23 -0700

    My second commit

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * add_node - adds a new node at the beginning of list_t list
+ * @head: head of a singly linked list
+ * @str: string to be stored in the list
+ * Return: address of the new element which is stored in head
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+	size_t numchar;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->str = strdup(str);
+
+	for (numchar = 0; str[numchar] != '\0'; numchar++)
+		;
+
+	new->len = numchar;
+	num->str = *head;
+	*head = new;
+
+	return (*head);
+}
