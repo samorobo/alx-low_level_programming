407a5a3e89278e47d112824fe38fdfa939f17bc2
‘samorobo’ <samgodwin111@gmail.com>
2022-05-24 09:46:39 -0700

    My zeroth commit

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * print_list - prints all the element oof a list_t list
+ * @h: single linked list
+ * Return: number of elements in the list
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t num_display;
+
+	for (num_display = 0; h != NULL; num_display++)
+	{
+		if (h->str == NULL)
+		{
+			printf("[%d] %s\n", 0, "(nill)");
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+			h = h->next;
+		}
+	}
+	return (num_display);
+}
