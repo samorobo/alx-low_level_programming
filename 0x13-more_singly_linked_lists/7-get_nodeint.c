98cc6b6863d840a26e26d3846d2ca5913226ae5b
‘samorobo’ <samgodwin111@gmail.com>
2022-05-26 15:04:08 -0700

    My seventh commit

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -9,14 +9,14 @@
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	int count = 0;
+	unsigned int count = 0;
 	listint_t *temp = head;
 
-	while (temp && count < index)
+	while (temp != NULL)
 	{
-		temp = temp->next;
+		if (count == index)
+			temp = temp->next;
 		count++;
 	}
-
-	return (temp ? temp : NULL);
+	return (temp->next);
 }
