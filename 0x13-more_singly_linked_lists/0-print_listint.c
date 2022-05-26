8008550122c1f4f39cf11b97e87ca23557bf1b16
‘samorobo’ <samgodwin111@gmail.com>
2022-05-26 05:47:16 -0700

    My zeroth commit

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints all the element of a listint_t list
+ * @h: head of a list
+ * Return: number of nodes
+ */
+
+size_t print_listint(coonst listint_t *h)
+{
+	size_t num = 0;
+
+	while (h != NULL)
+	{
+		printf("%d", h->n);
+		h = h->next;
+		num++;
+	}
+	return (num);
+}
