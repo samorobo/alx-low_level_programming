60cda7481a8ce3753cf444facb7f84682e4a6bfa
‘samorobo’ <samgodwin111@gmail.com>
2022-05-30 16:22:51 -0700

    My fifth commit

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,21 +1,19 @@
 #include "main.h"
-
 /**
- * flip_bits - functin that returns the number of bits you woould need to flip
- * to get from one number to another
+ * flip_bits - function that returns the number of bits you would need to flip
+ * to get from one number to anotherr
  * @n: number
  * @m: long int
  * Return: Number of bits
  */
-
-unsigned int flip_bits(unsigned long int n, unsigned int m)
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned long innt swap = n ^ m;
+	unsigned long int swap = n ^ m;
 	int counter = 0;
 
 	while (swap)
 	{
-		swap swap & (swap - 1);
+		swap = swap & (swap - 1);
 		counter++;
 	}
 
