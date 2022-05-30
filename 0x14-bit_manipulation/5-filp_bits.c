d2dd0a4454203b36cadfe360ce7ca2454622ea85
‘samorobo’ <samgodwin111@gmail.com>
2022-05-30 15:18:06 -0700

    My fifth commit

--- 0x14-bit_manipulation/5-filp_bits.c
+++ 0x14-bit_manipulation/5-filp_bits.c
@@ -1,25 +1,22 @@
-#include <stdlib.h>
 #include "main.h"
 
 /**
-  * flip_bits - ...
-  * @n: ...
-  * @m: ...
-  *
-  * Return: ...
-  */
+ * flip_bits - Counts the number of bits needed to be
+ *             flipped to get from one number to another.
+ * @n: The number.
+ * @m: The number to flip n to.
+ *
+ * Return: The necessary number of bits to flip to get from n to m.
+ */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned long int mask = 0, count = 0;
+	unsigned long int xor = n ^ m, bits = 0;
 
-	mask = n ^ m;
-	while (mask)
+	while (xor > 0)
 	{
-		if (mask & 1)
-			count++;
-
-		mask >>= 1;
+		bits += (xor & 1);
+		xor >>= 1;
 	}
 
-	return (count);
+	return (bits);
 }
