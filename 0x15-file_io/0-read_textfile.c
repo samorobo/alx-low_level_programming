31575d52006a492aaab84664bf7ce294c522d015
‘samorobo’ <samgodwin111@gmail.com>
2022-06-01 09:44:44 -0700

    My zeroth commit

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,22 +1,22 @@
 #include "main.h"
 
 /**
- * read_textfile - reads a textfile and prints it to the the posix standard output
- * @filename: pointer too the text in a file
- * @letters: number of letters its should read and print
- * Return: number of letters that could be read and printed, if it fails return 0
+ * read_textfile - reads a text file and prints the letters
+ * @filename: filename.
+ * @letters: numbers of letters printed.
+ *
+ * Return: numbers of letters printed. It fails, returns 0.
  */
-
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
-	ssize_t nrd, amt;
+	ssize_t nrd, nwr;
 	char *buf;
 
-	if (filename == NULL)
+	if (!filename)
 		return (0);
 
-	fd = open(filename, 0_RDONLY);
+	fd = open(filename, O_RDONLY);
 
 	if (fd == -1)
 		return (0);
@@ -26,11 +26,11 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 
 	nrd = read(fd, buf, letters);
-	amt = write(STDOUT_FILENO, buf, nrd);
+	nwr = write(STDOUT_FILENO, buf, nrd);
 
 	close(fd);
 
 	free(buf);
 
-	return (amt);
+	return (nwr);
 }
