4d3796e4653426f2d650aef1e8d599738dbfa99b
‘samorobo’ <samgodwin111@gmail.com>
2022-06-01 15:40:40 -0700

    My third commit

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,43 +2,41 @@
 #include <stdio.h>
 
 /**
- * error_file - checks if the file can be opened
- * @file_from: file to copy from
- * @file_to: file to be copied into
- * @argv: arguments vector
- * Return: no return
+ * error_file - checks if files can be opened.
+ * @file_from: file_from.
+ * @file_to: file_to.
+ * @argv: arguments vector.
+ * Return: no return.
  */
-
 void error_file(int file_from, int file_to, char *argv[])
 {
 	if (file_from == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: can't read from file %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	if (file_to == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: can't write to %s\n", argv[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
 }
 
 /**
- * main - check the code for ALX school students
- * @argc: number of arguments
- * @argv: argument vector
- * Return: Always 0
+ * main - check the code for Holberton School students.
+ * @argc: number of arguments.
+ * @argv: arguments vector.
+ * Return: Always 0.
  */
-
-int main(int argc, char argv[])
+int main(int argc, char *argv[])
 {
 	int file_from, file_to, err_close;
 	ssize_t nchars, nwr;
-	char buf[10243];
+	char buf[1024];
 
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO "s\n",  "Usage: cp file_from file_to");
+		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
 		exit(97);
 	}
 
