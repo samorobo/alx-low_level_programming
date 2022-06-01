9d39306e31238e47d0e379e31a2339316967f4db
‘samorobo’ <samgodwin111@gmail.com>
2022-06-01 09:35:45 -0700

    My zeroth commit

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -26,7 +26,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 
 	nrd = read(fd, buf, letters);
-	amt = write(STDOUT_FILENO, buf, letters);
+	amt = write(STDOUT_FILENO, buf, nrd);
 
 	close(fd);
 
