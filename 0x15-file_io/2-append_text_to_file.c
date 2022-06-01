7d18c91dc9a4abc3494f398ae72ffc2b16df041d
‘samorobo’ <samgodwin111@gmail.com>
2022-06-01 14:18:13 -0700

    My second commit

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -2,31 +2,32 @@
 
 /**
  * append_text_to_file - appends text at the end of a file
- * @filename: filename
- * @text_content: added content
- * Return: return 1 if the file exist, -1 if the file does not exist
- * or if it fails
+ * @filename: filename.
+ * @text_content: added content.
+ *
+ * Return: 1 if the file exists. -1 if the fails does not exist
+ * or if it fails.
  */
-
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd;
-	int num_letters;
+	int nletters;
 	int rwr;
 
 	if (!filename)
 		return (-1);
 
-	fd = open(filename, 0_WRONLY | 0_APPEND);
+	fd = open(filename, O_WRONLY | O_APPEND);
 
 	if (fd == -1)
 		return (-1);
 
-	if (text_content != NULL)
+	if (text_content)
 	{
-		for (num_letters = 0; text_content[num_letters]; num_letters++)
+		for (nletters = 0; text_content[nletters]; nletters++)
 			;
-		rwr = write(fd, text_content, num_letters);
+
+		rwr = write(fd, text_content, nletters);
 
 		if (rwr == -1)
 			return (-1);
