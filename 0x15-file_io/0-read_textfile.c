bf6687c80771a7bc8aa3099a4206d70d64a6d367
‘samorobo’ <samgodwin111@gmail.com>
2022-06-01 09:34:15 -0700

    My zeroth commit

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a textfile and prints it to the the posix standard output
+ * @filename: pointer too the text in a file
+ * @letters: number of letters its should read and print
+ * Return: number of letters that could be read and printed, if it fails return 0
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t nrd, amt;
+	char *buf;
+
+	if (filename == NULL)
+		return (0);
+
+	fd = open(filename, 0_RDONLY);
+
+	if (fd == -1)
+		return (0);
+
+	buf = malloc(sizeof(char) * (letters));
+	if (!buf)
+		return (0);
+
+	nrd = read(fd, buf, letters);
+	amt = write(STDOUT_FILENO, buf, letters);
+
+	close(fd);
+
+	free(buf);
+
+	return (amt);
+}
